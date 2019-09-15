01cd68f37d8d4e095c252b22ed23eb06e21534f1
Rubén Héctor García <raiben@gmail.com>
2019-09-15 13:35:35 +0200

    Version 1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tropescraper',
-      version='0.1',
+      version='1.0',
       description='A TvTropes scrapper',
       url='https://github.com/raiben/tropescraper',
       author='Rubén H. García',
